5c6e339cd3605423ef716b80ba87f42253ad2e67
santosh <rsantoshkumar447@gmail.com>
2025-05-02 20:46:06 +0530

    version1

---
+++ program_one.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class program_one {
+
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n=scan.nextInt();
+        int[] arr=new int[n];
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i]=scan.nextInt();
+        }
+        printAllPairs(arr);
+
+   
+    }
+
+public static void printAllPairs(int arr[])
+{
+    for(int i=0;i<arr.length;i++)
+    {
+        for(int j=i+1;j<arr.length;j++)
+        {
+            System.out.println(arr[i]+" "+arr[j]);
+        }
+    }
+}
+}
